643f05c06423069941bfd7671af290a7caf062b4
Elwin Stephan <elwin.stephan@gmail.com>
2020-07-01 15:17:38 +0200

    Remove dead code
    GitOrigin-RevId: 5564f1fd0ec5d736ee82f35ffd031f02f50014be

--- cmd/root.go
+++ cmd/root.go
@@ -1,15 +1,9 @@
 package cmd
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"fmt"
-	"io/ioutil"
 	"os"
-	"time"
 
-	"github.com/Shopify/sarama"
-	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -95,102 +89,4 @@ func initConfig() {
 		fmt.Println("Can't read config:", err)
 		os.Exit(1)
 	}
-}
-
-func getTLSConfig() (*tls.Config, error) {
-	certFile := viper.GetString("tls.cert")
-	keyFile := viper.GetString("tls.key")
-	caFile := viper.GetString("tls.caCert")
-	if certFile == "" && keyFile == "" && caFile == "" {
-		return nil, nil
-	}
-
-	// Load client cert
-	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
-	if err != nil {
-		return nil, err
-	}
-
-	// Load CA cert
-	caCert, err := ioutil.ReadFile(caFile)
-	if err != nil {
-		return nil, err
-	}
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(caCert)
-
-	// Compile the TLS Config
-	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{cert},
-		RootCAs:      caCertPool,
-	}
-
-	tlsConfig.BuildNameToCertificate()
-
-	return tlsConfig, nil
-}
-
-func getBrokers() ([]string, error) {
-	brokerList := viper.GetStringSlice("brokers")
-	if len(brokerList) == 0 {
-		return nil, errors.New("no brokers specified")
-	}
-
-	return brokerList, nil
-}
-
-func getKafkaVersion() (sarama.KafkaVersion, error) {
-	valueFromFile := viper.GetString("kafka_version")
-
-	// If there is a version provided in the config file then parse it and return it
-	if valueFromFile != "" {
-		parsedVersion, err := sarama.ParseKafkaVersion(valueFromFile)
-		if err != nil {
-			return sarama.KafkaVersion{}, fmt.Errorf("error when parsing kafka version config value: %s", err)
-		}
-		return parsedVersion, nil
-	}
-
-	// If version is not provided then return the default v1.
-	// Some features will not be available (e.g. ACL resource pattern types)
-	return sarama.V1_0_0_0, nil
-}
-
-func getConfig() (*sarama.Config, error) {
-	conf := sarama.NewConfig()
-	version, err := getKafkaVersion()
-	if err != nil {
-		return nil, fmt.Errorf("could not set the kafka version: %s", err)
-	}
-	conf.Version = version
-	conf.Consumer.Return.Errors = true
-	conf.Admin.Timeout = 20 * time.Second
-
-	tlsConfig, err := getTLSConfig()
-	if err != nil {
-		return nil, fmt.Errorf("cannot get TLS conf: %s", err)
-	}
-
-	if tlsConfig != nil {
-		conf.Net.TLS.Enable = true
-		conf.Net.TLS.Config = tlsConfig
-	}
-
-	return conf, err
-}
-
-// getKafkaClient returns a new client
-func getKafkaClient() (sarama.Client, error) {
-	conf, err := getConfig()
-	if err != nil {
-		return nil, fmt.Errorf("getKafkaClient: cannot get configuration: %v", err)
-	}
-
-	brokerList, err := getBrokers()
-	if err != nil {
-		return nil, err
-	}
-
-	// Create new client
-	return sarama.NewClient(brokerList, conf)
 }
